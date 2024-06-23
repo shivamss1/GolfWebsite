de8e646eaa5d29405b1933b31b6b5e20036ed57a
shivamss1 <shivamlimbhare9@gmail.com>
2024-06-23 15:02:36 +0530

    added aboutus and green div and card section

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ document.addEventListener("mousemove", function (dets) {
     blur1.style.top=dets.y-200+"px"
 })
 
-
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) { 
+    elem.addEventListener("mouseenter", function () {
+        csr.style.scale = 3
+        csr.style.border = "1px solid #fff"
+        csr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave", function () { 
+        csr.style.scale = 1
+        csr.style.border = "0px solid #95C11E"
+        csr.style.backgroundColor="#95C11E"
+    })
+})
 
 
 gsap.to("#nav", {
@@ -34,4 +46,18 @@ gsap.to("#main", {
         end: "top -70%",
         scrub:1,
     }
+})
+
+gsap.from("#about-us img,#about-us-in", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: "#about-us",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 58%",
+        scrub:7,
+    }    
 })
