c9a4d1d498e82270aa69ac8795365c4d85ebf92b
shivamss1 <shivamlimbhare9@gmail.com>
2024-06-27 12:12:41 +0530

    final website look ready

--- script.js
+++ script.js
@@ -56,8 +56,85 @@ gsap.from("#about-us img,#about-us-in", {
     scrollTrigger: {
         trigger: "#about-us",
         scroller: "body",
-        start: "top 60%",
-        end: "top 58%",
+        start: "top 70%",
+        end: "top 65%",
         scrub:7,
     }    
+})
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity: 0,
+    duration:1,
+    scrollTrigger: {
+        trigger: ".card",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub:1,
+    }
+})
+
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub:4,
+        
+    }
+})
+
+gsap.from("#colon2", {
+    x: 70,
+    y: 70,
+    scrollTrigger: {
+        trigger: "#colon2",
+        scroller: "body",
+        start: "top 100%",
+        end: "top 100%",
+        scrub:4,
+    }
+})
+
+gsap.to("#page4 h1", {
+    y: -37,
+    scrollTrigger: {
+        trigger: "#page4 h1",
+        scroller: "body", 
+        start: "top 95%",
+        end: "top 95%",
+        scrub:2.5,
+    }
+})
+
+var h2all = document.querySelectorAll("#box1 h2,#address h2");
+h2all.forEach(function (elem) {
+    elem.addEventListener("mouseenter",function() {
+        csr.style.scale = 3
+        csr.style.border = "1px solid #fff"
+        csr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function () { 
+        csr.style.scale = 1
+        csr.style.border = "1px solid #95C11E"
+        csr.style.backgroundColor="#95C11E"
+    })
+})
+
+var h5all = document.querySelectorAll("#box2 .imp");
+h5all.forEach(function (elem) { 
+    elem.addEventListener("mouseenter", function () { 
+        csr.style.scale = 3
+        csr.style.border = "1px solid #fff"
+        csr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function () { 
+        csr.style.scale = 1
+        csr.style.border = "1px solid #95C11E"
+        csr.style.backgroundColor="#95C11E"
+    })
 })
